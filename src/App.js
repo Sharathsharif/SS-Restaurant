5c5e696593a1c87cf7f6dfe6eae804fa57ce6f01
Sharath <sharathsharif@gmail.com>
2024-01-14 15:59:03 +0530

    created the loading page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import PreLoader from './Components/Preloader';
+import Landing from '../src/Animations/Landing'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PreLoader />
+     
+        <Landing />
+
+
+
+      
     </div>
+
+
   );
 }
 
 export default App;
+ 
