84942bb19f97e4292ab6900cdf9e83b9f6dd1758
Sharath <sharathsharif@gmail.com>
2024-01-23 19:20:01 +0530

    menu list updated and also created the built package

--- src/Components/Contactinformation.jsx
+++ src/Components/Contactinformation.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import './Pages/Home.css'
 export function Contactinfo(){
     return(
 
         <div className="d-flex flex-column align-item-centre">
 
             <h2 className="d-flex flex-column align-items-center">
-                Our Shop
+                Our Shop Location
             </h2>
 
-            <p className="mb-5">
+            <p className="mb-5 d-flex" >
                 15-12F , Northstreet,Marthandam, Kaniyakumari
             </p>
 
-            <h3>
-                Opening Hours
+            <h3 className=" m-5">
+                Opening Hours:
             </h3>
-            <p className="m-0">
-                Mon-fri : 9.30am to 9.30pm 
+            <p className="contactpara ">
+                     Mon - fri : 9.30am to 9.30pm 
             </p>
-            <p>
+            <p className="contactpara ">
                 Sat & Sun: 9.30am to 10.30pm
             </p>
         </div>
--- src/Components/Pages/Menu.jsx
+++ src/Components/Pages/Menu.jsx
@@ -4,6 +4,10 @@ import { Card, CardBody, CardText, CardTitle } from "react-bootstrap";
 import Breakfastimag from '../Image/gallery5.jpg'
 import Tamilnadu_Lunch from '../Image/Tamilnadu_Lunch.jpg'
 import Biriyani from '../Image/Biriyani.jpg'
+import chickenNoodles from '../Image/ChickenNoodles.jpg'
+import freshJuice from '../Image/freshJuice.jpg'
+import iceCream from '../Image/Dessert.jpg'
+import Gulab from '../Image/Gulab Jam.jpg'
 
 
 
@@ -16,15 +20,15 @@ const Breakfast = [
     },
     {
         id: 2,
-        name: 'puttu combo',
+        name: 'puttu ',
         Description: 'Rice, chicken, papadam, pachadi ',
-        price: "₹150/-"
+        price: "₹35/-"
     },
     {
         id: 3,
-        name: 'iddli combo',
+        name: 'iddli ',
         Description: 'Rice, vadai,sambar, pachadi ',
-        price: "₹150/-"
+        price: "₹10/-"
     },
     {
         id: 4,
@@ -58,42 +62,87 @@ const Lunch = [
 const Dinner = [
     {
         id: 1,
-        name: 'Porotta combo',
+        name: 'Porotta ',
         description: 'Rice, vadai,sambar, pachadi ',
+        price: "₹10/-"
+    },
+    {
+        id: 2,
+        name: 'idi Appam ',
+        description: ' ',
+        price: "₹15/-"
+    },
+    {
+        id: 3,
+        name: 'Chicken Biriyani',
+        description: ' ',
+        price: "₹150/-"
+    }
+]
+
+const FastFood = [
+    {
+        id: 1,
+        name: 'Chicken Noodles',
+        description: 'nil ',
         price: "₹150/-"
     },
     {
         id: 2,
-        name: 'idi Appam combo',
+        name: 'Chicken Rice',
         description: 'Rice, chicken, papadam, pachadi ',
         price: "₹150/-"
     },
     {
         id: 3,
-        name: 'Chicken Biriyani',
+        name: 'Burger',
         description: 'Rice, vadai,sambar, pachadi ',
-        price: "₹150/-"
+        price: "₹180/-"
+    },
+    {
+        id: 4,
+        name: 'french fries',
+        description: 'Rice, vadai,sambar, pachadi ',
+        price: "₹120/200/300/-"
     }
 ]
 
 const Dessert = [
     {
         id: 1,
-        name: 'Lemon Cake',
+        name: 'Gulab Jamun',
         description: 'flour, sugar, baking powder, lemon',
-        price: "₹150/-"
+        price: "₹50/-"
     },
     {
         id: 2,
-        name: 'Cinnamon Rolls',
+        name: 'Rasgulla',
         description: 'flour, salt, sugar, cinnamon, yeast, sour cream, milk',
-        price: "₹250/-"
+        price: "₹100/-"
     },
     {
         id: 3,
-        name: 'Vegan Pancakes',
+        name: 'Falooda',
         description: 'flour, sugar, baking powder, soya milk, blueberries',
-        price: "₹200/-"
+        price: "₹120/-"
+    },
+    {
+        id: 4,
+        name: 'Kulfi',
+        description: 'flour, sugar, baking powder, soya milk, blueberries',
+        price: "₹80/-"
+    },
+    {
+        id: 5,
+        name: 'Mysore Pak',
+        description: 'flour, sugar, baking powder, soya milk, blueberries',
+        price: "₹40/-"
+    },
+    {
+        id: 6,
+        name: 'Lassi',
+        description: 'flour, sugar, baking powder, soya milk, blueberries',
+        price: "₹80/-"
     }
 ]
 
@@ -102,25 +151,25 @@ const Juice = [
         id: 1,
         name: 'Fresh Juice',
         description: 'flour, sugar, baking powder, lemon',
-        price: "₹150/-"
+        price: "₹80/-"
     },
     {
         id: 2,
         name: 'Mohster shake',
         description: 'flour, sugar, baking powder, soya milk, blueberries',
-        price: "₹200/-"
+        price: "₹180/-"
     },
     {
         id: 3,
         name: 'Sharja',
         description: 'flour, salt, sugar, cinnamon, yeast, sour cream, milk',
-        price: "₹250/-"
+        price: "₹80/-"
     },
     {
         id: 4,
         name: 'Milkshake',
         description: 'flour, sugar, baking powder, soya milk, blueberries',
-        price: "₹200/-"
+        price: "₹100/-"
     }
 ]
 
@@ -155,10 +204,10 @@ export default function Menu() {
                                             <CardTitle className="text-center fs-2">
                                                 {Breakfast.name}
                                             </CardTitle>
-                                            <CardText className="text-center fs-4">
+                                            <CardText className="text-center text-success fs-4">
                                                 {Breakfast.description}
                                             </CardText>
-                                            <CardText className="text-center fs-3 fw-bold text-success">
+                                            <CardText className="text-center fs-3 fw-bold text-danger">
                                                 {Breakfast.price}
                                             </CardText>
                                         </CardBody>
@@ -230,7 +279,7 @@ export default function Menu() {
                                             <CardTitle className="text-center fs-2">
                                                 {Dinner.name}
                                             </CardTitle>
-                                            <CardText className="text-center fs-4">
+                                            <CardText className="text-center  fs-4">
                                                 {Dinner.Description}
                                             </CardText>
                                             <CardText className="text-center fs-3 fw-bold">
@@ -249,7 +298,85 @@ export default function Menu() {
 
 
 
-            {/*  Lunch list */}
+{/*  Fast Food list */}
+
+
+<div className="lunch  text-light bg-dark py-5">
+                <div className="container">
+                    <h2 className="text-center fs-1 mb-4 mb-lg-5 text-uppercase fw-bold text-danger">
+                        Fast Food
+                    </h2>
+
+                    <div className="row flex-column flex-lg-row ">
+
+                        <div className="col-lg-6 d-flex flex-column  justify-content-around">
+                            {FastFood.map((FastFood) => (
+                                <div key={FastFood.id}>
+                                    <Card className="border-0 bg-dark text-light">
+                                        <CardBody>
+                                            <CardTitle className="text-center fs-2">
+                                                {FastFood.name}
+                                            </CardTitle>
+                                            {/* <CardText className="text-center fs-4">
+                                                {Dessert.description}
+                                            </CardText> */}
+                                            <CardText className="text-center fs-3 fw-bold text-success">
+                                                {FastFood.price}
+                                            </CardText>
+                                        </CardBody>
+                                    </Card>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="col-lg-6 d-flex justify-content-center">
+                            <img src={chickenNoodles} className="img-fluid w-75 mt-4 mt-lg-0 " alt="" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+ {/*  Juice list */}
+
+ <div className="Breakfast py-5">
+                <div className="container">
+                    <h2 className="text-center fs-1 mb-4 mb-lg-5  fw-bold text-danger">
+                        Juice
+                    </h2>
+                    <div className="row flex-column-reverse flex-lg-row ">
+                        <div className="col-lg-6 d-flex justify-content-center">
+                            <img src={freshJuice} className="img-fluid w-75 mt-4 mt-lg-0 " alt="" />
+                        </div>
+                        <div className="col-lg-6 d-flex flex-column justify-content-around">
+                            {Juice.map((Juice) => (
+                                <div key={Juice.id}>
+                                    <Card className="border-0">
+                                        <CardBody>
+                                            <CardTitle className="text-center fs-2">
+                                                {Juice.name}
+                                            </CardTitle>
+                                            {/* <CardText className="text-center fs-4">
+                                                {Juice.description}
+                                            </CardText> */}
+                                            <CardText className="text-center fs-3 fw-bold text-success">
+                                                {Juice.price}
+                                            </CardText>
+                                        </CardBody>
+                                    </Card>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+            {/*  Dessert list */}
 
 
             <div className="lunch  text-light bg-dark py-5">
@@ -280,7 +407,7 @@ export default function Menu() {
                             ))}
                         </div>
                         <div className="col-lg-6 d-flex justify-content-center">
-                            <img src={Breakfastimag} className="img-fluid w-75 mt-4 mt-lg-0 " alt="" />
+                            <img src={Gulab} className="img-fluid w-75 mt-4 mt-lg-0 " alt="" />
                         </div>
 
                     </div>
@@ -291,40 +418,7 @@ export default function Menu() {
 
 
 
-            {/*  Juice list */}
-
-            <div className="Breakfast py-5">
-                <div className="container">
-                    <h2 className="text-center fs-1 mb-4 mb-lg-5  fw-bold text-danger">
-                        Juice
-                    </h2>
-                    <div className="row flex-column-reverse flex-lg-row ">
-                        <div className="col-lg-6 d-flex justify-content-center">
-                            <img src={Breakfastimag} className="img-fluid w-75 mt-4 mt-lg-0 " alt="" />
-                        </div>
-                        <div className="col-lg-6 d-flex flex-column justify-content-around">
-                            {Juice.map((Juice) => (
-                                <div key={Juice.id}>
-                                    <Card className="border-0">
-                                        <CardBody>
-                                            <CardTitle className="text-center fs-2">
-                                                {Juice.name}
-                                            </CardTitle>
-                                            {/* <CardText className="text-center fs-4">
-                                                {Juice.description}
-                                            </CardText> */}
-                                            <CardText className="text-center fs-3 fw-bold text-success">
-                                                {Juice.price}
-                                            </CardText>
-                                        </CardBody>
-                                    </Card>
-                                </div>
-                            ))}
-                        </div>
-
-                    </div>
-                </div>
-            </div>
+           
 
 
         </div>
