bb17279d94cc95e645d3a47267686cf2a756f16f
Sharath <sharathsharif@gmail.com>
2024-01-23 20:01:10 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbarpage from './Components/Navbar';
 import Footerpage from './Components/Footer';
-import Home from './Components/Pages/Home';
+
 
 function App() {
   return (
