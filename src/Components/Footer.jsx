2c520d28f0a80bdefbd3993b8cb404d376e404b4
Sharath <sharathsharif@gmail.com>
2024-01-30 15:01:28 +0530

    Clean up GitHub Pages branch

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Footerpage(){
-    return(
-        <footer className="bg-body-tertiary ">
-            <p className="p-3 m-0 text-center ">Copyright@ This site was made by Sharath</p>
-        </footer>
-    )
-}
